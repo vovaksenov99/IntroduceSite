3ba71b7d43ef14e491ffd43f7d8565c0272080cd
vovaksenov99 <vovaaksenov99@mail.ru>
2017-11-23 00:29:27 +0300

    make it better

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express        = require('express');
 const app            = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+;
 express()
   .get('/', function(req, res) {
 	  res.send("Kek");
