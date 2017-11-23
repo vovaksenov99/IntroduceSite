db4e85f0728d65ebb484202fb04b54a57c5e918f
vovaksenov99 <vovaaksenov99@mail.ru>
2017-11-23 20:04:38 +0300

    init

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const PORT = process.env.PORT || 5000;
 
 express()
   .get('/', function(req, res) {
-	  res.send("Kek");
+	  res.send("Kek "+PORT);
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
