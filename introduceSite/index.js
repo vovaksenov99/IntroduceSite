012ec6da9903611240ef784f6b30981837da3464
vovaksenov99 <vovaaksenov99@mail.ru>
2017-11-23 22:51:45 +0300

    Main page with resources

--- introduceSite/index.js
+++ introduceSite/index.js
@@ -2,10 +2,11 @@ const express        = require('express');
 const app            = express();
 
 const PORT = process.env.PORT || 5000;
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public',express.static('introduceSite/public'));
 
-express()
+
+app
   .get('/', function(req, res) {
 	res.sendfile('introduceSite/index.html');
   })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
