161dc3568df5718ad82585ace6560d02544fc4b3
vovaksenov99 <vovaaksenov99@mail.ru>
2017-11-23 22:21:07 +0300

    Add public folder

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express        = require('express');
 const app            = express();
 
 const PORT = process.env.PORT || 5000;
+app.use(express.static(path.join(__dirname, 'public')));
 
 express()
   .get('/', function(req, res) {
